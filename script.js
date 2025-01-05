66ebe14c2243ee7fe82d89707380faf22d175490
VladimirB <vlbelevich@yandex.by>
2025-01-05 21:50:48 +0300

    Add steps to follow

---
+++ script.js
@@ -0,0 +1,18 @@
+const myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+//Write a function that loops through the array and displays each book on the page.
+//for first steps myLibrary should contain some books
+
+//Add a “New Book” button that brings up a form allowing users to input the details for the new book and add it to the library: author, title, number of pages, whether it’s been read and anything else you might want.
+
+//Add a button on each book’s display to remove the book from the library
+
+//Add a button on each book’s display to change its read status
