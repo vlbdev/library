ff7d8a85a8ae0a95205e4c37bbdded54db93251c
VladimirB <vlbelevich@yandex.by>
2025-01-07 12:54:01 +0300

    Add comments with remain tasks

--- script.js
+++ script.js
@@ -77,10 +77,7 @@ function addBookToLibrary() {
   // do stuff here
 }
 
-//Write a function that loops through the array and displays each book on the page.
-//for first steps myLibrary should contain some books
-
-//Add a “New Book” button that brings up a form allowing users to input the details for the new book and add it to the library: author, title, number of pages, whether it’s been read and anything else you might want.
+//create constructor for new book object
 
 //Add a button on each book’s display to remove the book from the library
 
